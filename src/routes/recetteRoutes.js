2c30cb8f95ad14fdc5308580a19ddcdff9dd3548
Mamoudou12 <bmamoudou463@gmail.com>
2024-09-15 19:51:59 +0000

    Adding recetteRoutes.js and defining routes

---
+++ src/routes/recetteRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { getAllRecipes, getRecipeById, createRecipe, updateRecipe, deleteRecipe } from '../controllers/recetteController.js';
+
+const router = express.Router();
+
+router.get('/recipes', getAllRecipes);
+router.get('/recipes/:id', getRecipeById);
+router.post('/recipes', createRecipe);
+router.put('/recipes/:id', updateRecipe);
+router.delete('/recipes/:id', deleteRecipe);
+
+export default router;
