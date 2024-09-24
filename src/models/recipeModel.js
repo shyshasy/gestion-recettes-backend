118f3fadd67c872d851e12c4b9006d76b03ea192
Mamoudou12 <bmamoudou463@gmail.com>
2024-09-24 09:42:55 +0000

    using models folder in our project

---
+++ src/models/recipeModel.js
@@ -0,0 +1,36 @@
+import db from '../config/db.js';
+
+class Recipe {
+    // Récupérer toutes les recettes
+    static async getAllRecipes() {
+        const [results] = await db.query('SELECT * FROM recipes');
+        return results;
+    }
+
+    // Récupérer une recette par ID
+    static async getRecipeById(id) {
+    const [results] = await db.query('SELECT * FROM recipes WHERE id = ?', [id]);
+    return results.length > 0 ? results[0] : null;
+}
+
+
+    // Créer une nouvelle recette
+    static async createRecipe(title, description, date) {
+        const [result] = await db.query('INSERT INTO recipes (title, description, date) VALUES (?, ?, ?)', [title, description, date]);
+        return result.insertId;
+    }
+
+    // Mettre à jour une recette
+    static async updateRecipe(id, title, description, date) {
+        const [result] = await db.query('UPDATE recipes SET title = ?, description = ?, date = ? WHERE id = ?', [title, description, date, id]);
+        return result.affectedRows;
+    }
+
+    // Supprimer une recette
+    static async deleteRecipe(id) {
+        const [result] = await db.query('DELETE FROM recipes WHERE id = ?', [id]);
+        return result.affectedRows;
+    }
+}
+
+export default Recipe;
